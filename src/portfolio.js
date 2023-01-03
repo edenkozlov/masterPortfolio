7c02ac2985eca83e5538be003afc4bc895a698f9
edenkozlov <96761592+edenkozlov@users.noreply.github.com>
2023-01-03 01:08:03 -0500

    small changes

--- src/portfolio.js
+++ src/portfolio.js
@@ -615,7 +615,7 @@ const projectsHeader = {
 };
 
 const publicationsHeader = {
-  title: "Publications",
+  title: "Testimonies",
   description:
     "I have worked on and published a few research papers and publications of my own.",
   avatar_image_path: "projects_image.svg",
@@ -672,12 +672,12 @@ const contactPageData = {
     title: "Contact Us",
     profile_image_path: "animated_ashutosh.png",
     description:
-      "I am available on almost every social media. You can message me, I will reply within 24 hours. I can help you with ML, AI, React, Android, Cloud and Opensource Development.",
+      "We are available on the social media platforms bellow. You may also book an appointment with one of our experts, we will get back to you within 24h!",
   },
   blogSection: {
-    title: "Blogs",
+    title: "Thank you",
     subtitle:
-      "For individual fundamental empowerment, I like to write powerful lessons that create impact on each of the reader individually to change the core of their character.",
+      "From the entire Luna Studios team, we'd like to say thank you for trusting us to help create your dream project!",
     link: "https://ashutoshhathidara.wordpress.com",
     avatar_image_path: "blogs_image.svg",
   },
