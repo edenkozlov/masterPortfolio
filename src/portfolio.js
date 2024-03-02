0fe70b32c67c9feff9985c6229875834661e4508
edenkozlov <96761592+edenkozlov@users.noreply.github.com>
2024-03-02 15:08:39 -0500

    final

--- src/portfolio.js
+++ src/portfolio.js
@@ -41,7 +41,7 @@ const socialMediaLinks = [
 
   {
     name: "Gmail",
-    link: "mailto:0xlunastudios@gmail.com",
+    link: "mailto:0x.lunastudiosss@gmail.com",
     fontAwesomeIcon: "fa-google", // Reference https://fontawesome.com/icons/google?style=brands
     backgroundColor: "#D14836", // Reference https://simpleicons.org/?q=gmail
   },
@@ -51,12 +51,12 @@ const socialMediaLinks = [
     fontAwesomeIcon: "fa-twitter", // Reference https://fontawesome.com/icons/twitter?style=brands
     backgroundColor: "#1DA1F2", // Reference https://simpleicons.org/?q=twitter
   },
-  {
-    name: "Instagram",
-    link: "https://www.instagram.com/0x_luna_studios/",
-    fontAwesomeIcon: "fa-instagram", // Reference https://fontawesome.com/icons/instagram?style=brands
-    backgroundColor: "#E4405F", // Reference https://simpleicons.org/?q=instagram
-  },
+  //{
+  //  name: "Instagram",
+  // link: "https://www.instagram.com/0x_luna_studios/",
+  //  fontAwesomeIcon: "fa-instagram", // Reference https://fontawesome.com/icons/instagram?style=brands
+  //  backgroundColor: "#E4405F", // Reference https://simpleicons.org/?q=instagram
+  //},
   {
     name: "Fiverr",
     link:
@@ -611,7 +611,7 @@ const contactPageData = {
   },
   addressSection: {
     title: "Email:",
-    subtitle: "0xlunastudios@gmail.com",
+    subtitle: "0x.lunastudiosss@gmail.com",
     avatar_image_path: "address_image.svg",
     location_map_link: "https://goo.gl/maps/MpMqtRZytFchMkZ76",
   },
